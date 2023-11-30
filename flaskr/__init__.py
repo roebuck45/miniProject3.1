ca9823d4ef438fe61e27a81c3c23a129c71e3028
roebuck45 <j_seirer@mail.fhsu.edu>
2023-11-30 15:47:05 -0600

    setup data based, blueprints, regestar

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -28,5 +29,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
